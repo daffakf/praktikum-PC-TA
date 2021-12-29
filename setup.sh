a0e4541f0ed1a888dbac217a1e3c1a471b684fe6
Daffa Ksatria Firdaus <daffakf123@gmail.com>
2021-12-29 15:19:17 +0700

    added ikan badut.jpg dan setup.h untuk tugas akhir

---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt -qq install figlet
+figlet -f slant "Kelompok 3"
+echo 'Kelompok 3'
+echo ' '
+echo '065119165 Fauzan Dzaki Andrian'
+echo '065119166 Muhammad Farchan Ramdhani'
+echo '065119168 Daffa Ksatria Firdaus'
+echo ' '
+echo "Menginstall dan Mengupdate Opencv..."
+pip install opencv-python
+
+# Done
+echo "Selesai"
