8515d985eb7dc94ab5587d25d9aa8a2a89409f4e
Daffa Ksatria Firdaus <daffakf123@gmail.com>
2021-12-29 16:21:38 +0700

    update setup.sh , memperbaiki penulisan code

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 # Banner
 sudo apt -qq install figlet
 figlet -f slant "Kelompok 3"
-echo 'Kelompok 3'
-echo ' '
-echo '065119165 Fauzan Dzaki Andrian'
-echo '065119166 Muhammad Farchan Ramdhani'
-echo '065119168 Daffa Ksatria Firdaus'
-echo '' 
+echo "Kelompok 3"
+echo " " 
+echo "065119165 Fauzan Dzaki Andrian"
+echo "065119166 Muhammad Farchan Ramdhani"
+echo "065119168 Daffa Ksatria Firdaus"
+echo "" 
 
 # Update Library
 echo "Menginstall dan Mengupdate..."
-echo ''
+echo " "
 pip install --quiet --upgrade scikit-image
 
 # Download sample file
 echo "Mendownload file contoh..."
-echo ''
-wget --quiet -o badut.jpg "https://docs.google.com/uc?export=download&id=1NoeVl0K-6YkBoffz6dsqaogwruJjaa6z"
+echo " "
+wget --quiet -O badut.jpg "https://docs.google.com/uc?export=download&id=1NoeVl0K-6YkBoffz6dsqaogwruJjaa6z"
 
 # Done
 echo "Selesai"
